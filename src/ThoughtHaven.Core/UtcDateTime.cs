667d8bb702b0d2d6a82e62d660a0ecca60cf5c80
Brendan Ridenour <Journeyman979@hotmail.com>
2019-12-24 20:34:41 -0500

    Add UtcDateTime

---
+++ src/ThoughtHaven.Core/UtcDateTime.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ThoughtHaven
+{
+    public class UtcDateTime
+    {
+        public long Ticks { get; }
+
+        protected DateTimeOffset Offset { get; }
+        public DayOfWeek DayOfWeek => this.Offset.DayOfWeek;
+        public int Year => this.Offset.Year;
+        public int Month => this.Offset.Month;
+        public int Day => this.Offset.Day;
+        public int Hour => this.Offset.Hour;
+        public int Minute => this.Offset.Minute;
+
+        public UtcDateTime(long ticks)
+        {
+            this.Ticks = Guard.LessThan(nameof(ticks), ticks, minimum: 0);
+            this.Offset = new DateTimeOffset(this.Ticks, TimeSpan.Zero);
+        }
+
+        public DateTimeOffset ToOffset() => new DateTimeOffset(this.Ticks, TimeSpan.Zero);
+
+        public static implicit operator UtcDateTime(DateTimeOffset dateTime) =>
+            new UtcDateTime(dateTime.UtcTicks);
+
+        public static implicit operator DateTimeOffset(UtcDateTime dateTime) =>
+            dateTime.Offset;
+    }
+}
---
+++ test/ThoughtHaven.Core.Test/UtcDateTimeTests.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using Xunit;
+
+namespace ThoughtHaven
+{
+    public class UtcDateTimeTests
+    {
+        public class DayOfWeekProperty
+        {
+            public class GetOperator
+            {
+                [Theory]
+                [InlineData(1)]
+                [InlineData(1_000_000_000)]
+                public void WhenCalled_SetsDayOfWeek(long ticks)
+                {
+                    var offset = new DateTimeOffset(ticks, TimeSpan.Zero);
+                    var utc = new UtcDateTime(ticks);
+
+                    Assert.Equal(offset.DayOfWeek, utc.DayOfWeek);
+                }
+            }
+        }
+
+        public class YearProperty
+        {
+            public class GetOperator
+            {
+                [Theory]
+                [InlineData(1)]
+                [InlineData(1_000_000_000)]
+                public void WhenCalled_SetsYear(long ticks)
+                {
+                    var offset = new DateTimeOffset(ticks, TimeSpan.Zero);
+                    var utc = new UtcDateTime(ticks);
+
+                    Assert.Equal(offset.Year, utc.Year);
+                }
+            }
+        }
+
+        public class MonthProperty
+        {
+            public class GetOperator
+            {
+                [Theory]
+                [InlineData(1)]
+                [InlineData(1_000_000_000)]
+                public void WhenCalled_SetsMonth(long ticks)
+                {
+                    var offset = new DateTimeOffset(ticks, TimeSpan.Zero);
+                    var utc = new UtcDateTime(ticks);
+
+                    Assert.Equal(offset.Month, utc.Month);
+                }
+            }
+        }
+
+        public class DayProperty
+        {
+            public class GetOperator
+            {
+                [Theory]
+                [InlineData(1)]
+                [InlineData(1_000_000_000)]
+                public void WhenCalled_SetsDay(long ticks)
+                {
+                    var offset = new DateTimeOffset(ticks, TimeSpan.Zero);
+                    var utc = new UtcDateTime(ticks);
+
+                    Assert.Equal(offset.Day, utc.Day);
+                }
+            }
+        }
+
+        public class HourProperty
+        {
+            public class GetOperator
+            {
+                [Theory]
+                [InlineData(1)]
+                [InlineData(1_000_000_000)]
+                public void WhenCalled_SetsHour(long ticks)
+                {
+                    var offset = new DateTimeOffset(ticks, TimeSpan.Zero);
+                    var utc = new UtcDateTime(ticks);
+
+                    Assert.Equal(offset.Hour, utc.Hour);
+                }
+            }
+        }
+
+        public class MinuteProperty
+        {
+            public class GetOperator
+            {
+                [Theory]
+                [InlineData(1)]
+                [InlineData(1_000_000_000)]
+                public void WhenCalled_SetsMinute(long ticks)
+                {
+                    var offset = new DateTimeOffset(ticks, TimeSpan.Zero);
+                    var utc = new UtcDateTime(ticks);
+
+                    Assert.Equal(offset.Minute, utc.Minute);
+                }
+            }
+        }
+
+        public class Constructor
+        {
+            public class TicksOverload
+            {
+                [Fact]
+                public void TicksBelowZero_Throws()
+                {
+                    Assert.Throws<ArgumentOutOfRangeException>("ticks", () =>
+                    {
+                        new UtcDateTime(ticks: -1);
+                    });
+                }
+
+                [Theory]
+                [InlineData(1)]
+                [InlineData(2)]
+                public void WhenCalled_SetsTicks(long ticks)
+                {
+                    var utc = new UtcDateTime(ticks);
+
+                    Assert.Equal(ticks, utc.Ticks);
+                }
+            }
+        }
+
+        public class ToOffsetMethod
+        {
+            public class EmptyOverload
+            {
+                [Fact]
+                public void WhenCalled_ReturnsOffset()
+                {
+                    var offset = DateTimeOffset.UtcNow;
+                    var utc = new UtcDateTime(offset.UtcTicks);
+
+                    var result = utc.ToOffset();
+
+                    Assert.Equal(offset, result);
+                }
+            }
+        }
+
+        public class UtcDateTimeOperator
+        {
+            public class DateTimeOverload
+            {
+                [Fact]
+                public void WhenCalled_ReturnsOffset()
+                {
+                    var offset = DateTimeOffset.UtcNow;
+
+                    UtcDateTime utc = offset;
+
+                    Assert.Equal(offset.UtcTicks, utc.Ticks);
+                }
+            }
+        }
+
+        public class DateTimeOffsetOperator
+        {
+            public class DateTimeOverload
+            {
+                [Theory]
+                [InlineData(1)]
+                [InlineData(2)]
+                public void WhenCalled_ReturnsOffset(long ticks)
+                {
+                    var utc = new UtcDateTime(ticks);
+
+                    DateTimeOffset offset = utc;
+
+                    Assert.Equal(ticks, offset.UtcTicks);
+                }
+            }
+        }
+    }
+}
